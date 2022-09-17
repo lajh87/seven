7fc60f3e2eb85c390c1310c684d6971fc8d181eb
lheley <lajh87@live.co.uk>
2022-09-17 08:08:02 +0100

    osm

--- inst/milheat/app.R
+++ inst/milheat/app.R
@@ -1,35 +1,30 @@
 library(shiny)
 library(dplyr)
 library(magrittr)
+library(leaflet)
 
 establishments <- readr::read_csv("establishments.csv")
 
 ui <- navbarPage(
-  "milheat", id="nav",
-  
-  tabPanel("Interactive map",
-           div(class="outer",
-               tags$head(
-                 # Include our custom CSS
-                 includeCSS("styles.css"),
-                 includeScript("gomap.js")
-               ),
-               
-               # If not using custom CSS, set height of leafletOutput to a number instead of percent
-               leafletOutput("map", width="100%", height="100%"),
-               
-               # Shiny versions prior to 0.11 should use class = "modal" instead.
-               absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                             draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                             width = 330, height = "auto",
-                             
-                             h2("Explorer")
-               ),
-               
-               tags$div(id="cite",
-                        'Data compiled for ', tags$em('Defence Hackathon'), ' by team seven.'
-               )
-           )
+  title = "milheat", 
+  id="nav",
+  tabPanel(
+    title = "Interactive map",
+    div(class="outer",
+        tags$head(
+          includeCSS("styles.css"),
+          includeScript("gomap.js")
+        ),
+        leafletOutput("map", width="100%", height="100%"),
+        absolutePanel(
+          id = "controls", class = "panel panel-default", fixed = TRUE,
+          draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+          width = 330, height = "auto",
+          h2("Explorer")
+        ),
+        tags$div(id="cite", 'Data compiled for ', tags$em('Defence Hackathon'), ' by team seven.'
+        )
+    )
   ),
   
   tabPanel("Data explorer",
